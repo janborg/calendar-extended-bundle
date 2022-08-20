bb0a8c770496c4e67a8612a986917a78a3cbb624
Janborg <jan-github@luenborg.eu>
2022-08-20 12:29:22 +0200

    php8 Warnings -> isset

--- src/Resources/contao/classes/EventsExt.php
+++ src/Resources/contao/classes/EventsExt.php
@@ -284,7 +284,7 @@ class EventsExt extends Events
                         }
 
                         // check if there is any exception
-                        if (is_array($arrEventSkipInfo[$objEvents->id])) {
+                        if (isset($arrEventSkipInfo[$objEvents->id])) {
                             // modify the css class of the exceptions
                             $objEvents->cssClass = $masterCSSClass;
                             unset($objEvents->moveReason);
@@ -349,7 +349,7 @@ class EventsExt extends Events
                         // Skip events outside the scope
                         if ($objEvents->endTime < $intStart || $objEvents->startTime > $intEnd) {
                             // in case of a move we have to reset the original date
-                            if ($oldDate) {
+                            if (isset($oldDate)) {
                                 $objEvents->startTime = $oldDate['startTime'];
                                 $objEvents->endTime = $oldDate['endTime'];
                             }
@@ -390,7 +390,7 @@ class EventsExt extends Events
                         $objEvents->oldEndTime = null;
 
                         // in case of a move we have to reset the original date
-                        if ($oldDate) {
+                        if (isset($oldDate)) {
                             $objEvents->startTime = $oldDate['startTime'];
                             $objEvents->endTime = $oldDate['endTime'];
                         }
